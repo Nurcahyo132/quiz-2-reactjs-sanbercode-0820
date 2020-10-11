bb1c90c9d325fb3f3c905a87202f67c31835ff05
Nurcahyo132 <61648714+Nurcahyo132@users.noreply.github.com>
2020-10-11 15:03:30 +0700

    Quiz

---
+++ Soal 14/Volume.js
@@ -0,0 +1,23 @@
+const VolumeBalok  = (x,y,z) => {
+return x * y* z;
+}
+
+const x = 10;
+const y = 5;
+const z = 6;
+
+const perhitungan = `${x}`* `${y}`*`${z}`;
+
+console.log(perhitungan)
+
+const VolumeKubus = (a, b, c) =>{
+    return a * b* c;
+}
+
+const a = 5;
+const b = 5;
+const c = 5;
+
+const perhitungan1 = `${a}` * `${b}` * `${c}`;
+
+console.log(perhitungan1)
